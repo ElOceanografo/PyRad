501a09aba721a725682d27da56ec93e902f9a201
Sam Urmy <eloceanografo@gmail.com>
2014-05-15 10:12:12 -0400

    start modifying ScopeMonitorThread to use new classes

--- src/scope_monitor.py
+++ src/scope_monitor.py
@@ -5,18 +5,19 @@ import numpy as np
 
 class ScopeMonitorThread(threading.Thread):
 	"""docstring for ScopeMonitorThread"""
-	def __init__(self, data_q):
+	def __init__(self, data_q, radar_scope):
 		super(ScopeMonitorThread, self).__init__()
-
+		self.radar_scope = radar_scope
 		self.data_q = data_q
 		self.alive = threading.Event()
 		self.alive.set()
 
 	def run(self):
-		dt = 1.0 #2100.0
+		dt = 2.0 #2100.0
 		while self.alive.isSet():
 			timestamp = time.time()
-			self.data_q.put((timestamp, np.random.randn(128)))
+			self.radar_scope.capture_sweep()
+			self.data_q.put((timestamp, self.radar_scope.get_trimmed_sweep()))
 			time.sleep(dt)
 
 	def join(self, timeout=None):
